781cb8f99e96871e6c92b901d28bc6f0895e240b
shonenenenene <danya.sytaev@gmail.com>
2022-07-22 12:26:27 +0300

    validation is done

--- src/components/Cart/OrderForm/OrderForm.tsx
+++ src/components/Cart/OrderForm/OrderForm.tsx
@@ -1,11 +1,56 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 import { useTypedSelector } from '../../hooks/useTypedSelector'
 import './style.scss'
 
 const OrderForm: FC = () => {
 
   const currentCart = useTypedSelector(state => state.cartReducer.products)
-  const sum = currentCart.map((e) => {return e.price * e.count}).reduce((prev, curr) => prev + curr, 0)
+  const sum = currentCart.map((e) => { return e.price * e.count }).reduce((prev, curr) => prev + curr, 0)
+  
+  const [disabled, setDisabled] = useState(true)
+  const [mail, setMail] = useState('')
+  const [phone, setPhone] = useState('')
+  const [userName, setUserName] = useState('')
+  const [org, setOrg] = useState('')
+  const [validMail, setValidMail] = useState(true)
+  const valid = 'valid'
+
+  const nameHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const re = /[-0-9`~!@#$%^&*()_=+\\|\[\]{};:'",.<>\/?]/
+    if (!re.test(String(e.target.value))) setUserName(e.target.value) 
+  }
+
+  const mailHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+   setMail(e.target.value)
+  }
+
+  const phoneHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const re = /^\d+(-\d+)*$/
+    if ((re.test(String(e.target.value)) || e.target.value === '') && (e.target.value).toString().length <= 11) setPhone(e.target.value)
+  }
+
+  const orgHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setOrg(e.target.value)
+  }
+
+  useEffect(() => {
+    if ((/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(String(mail))) || mail === '') {
+      setValidMail(false)
+    } else {
+      setValidMail(true)
+    }
+  }, [mail, validMail])
+
+  useEffect(() => {
+    if (mail === ('') ||
+      !(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(String(mail))) ||
+      phone === ('') ||
+      userName === ('') ||
+      org === ('')) {
+      setDisabled(true)
+      } else setDisabled(false)
+
+  },[mail, phone, userName, org])
   
   return (
     <div className='order-form'>
@@ -13,17 +58,17 @@ const OrderForm: FC = () => {
       <form>
         <p className='cont-info'>Контактная информация</p>
 
-        <div className='full-name'><input  placeholder='ФИО'/></div>
-        <div className='phone'><input placeholder='Контактный телефон'/></div>
-        <div className='mail'><input placeholder='Email'/></div>
-        <div className='org'><input placeholder='Организация' /></div>
+        <div className='full-name'><input onChange={(e) => nameHandler(e)} value={userName}  placeholder='ФИО'/></div>
+        <div className='phone'><input onChange={(e) => phoneHandler(e)} value={phone} placeholder='Контактный телефон'/></div>
+        <div className={`mail ${validMail && valid}`}><input onChange={(e) => mailHandler(e)} value={mail} placeholder='Email'/></div>
+        <div className='org'><input onChange={(e) => orgHandler(e)} value={org} placeholder='Организация' /></div>
         
         <div className='order-total'>
           <div className='result'>
             <p>Итого</p>
             <div className='order-total-price'>{sum} руб.</div>
           </div>
-          <button>Оформить заказ</button>
+          <button disabled={disabled} type='button'>Оформить заказ</button>
           <div className='order-offer'>Коммерческое предложение</div>
         </div>
       </form>
